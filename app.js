c8bf0e18a20b7056b3483d07d2d31821fc0374d9
dramane1 <dramanediara@gmail.com>
2021-04-18 17:41:50 +0530

    header and footer fixe, contact form establish

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const homeStartingContent= "Lacus vel facilisis volutpat est velit egestas dui i
 const aboutContent = "Vi is a Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
@@ -20,6 +21,17 @@ app.get("/about", function(req,res){
 
 app.get("/contact",function(req,res){
     res.render("contact");
+})
+
+
+app.post("/contact",function(req,res){
+let name= req.body.name;
+let description= req.body.description;
+console.log(name);
+console.log(description);
+
+// res.redirect("/");
+
 })
 
 app.listen(4000, function(){
